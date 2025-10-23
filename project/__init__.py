9aa936b4db07b0e7a971224a1a5fd3e0dab55061
Pär Halje <par.halje@med.lu.se>
2025-10-23 15:38:02 +0200

    Environment and readme

--- project/__init__.py
+++ project/__init__.py
@@ -3,13 +3,11 @@ Package initialization file.
 """
 
 from .database import Database
-from .register_qtm_files import process_qtm_files
 from .project_structure import ProjectStructure
 from .paf_parser import parse_paf_file, ProjectSettings
 
 __all__ = [
     'Database',
-    'process_qtm_files',
     'parse_paf_file',
     'ProjectSettings',
     'ProjectStructure',
